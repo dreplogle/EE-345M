d6d8ef8d8c5baabb0d4854c4c6f3d48060ad88e0
Katy Loeffler <katy.loeffler@gmail.com>
2011-01-27 14:14:30 -0600

    ?

--- boards/ek-lm3s8962/uart_echo/uart_echo.c
+++ boards/ek-lm3s8962/uart_echo/uart_echo.c
@@ -154,8 +154,8 @@ main(void)
     //
     // Enable the peripherals used by this example.
     //
-    //SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    //SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     //
     // Enable processor interrupts.
@@ -165,25 +165,25 @@ main(void)
     //
     // Set GPIO A0 and A1 as UART pins.
     //
-    //GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     //
     // Configure the UART for 115,200, 8-N-1 operation.
     //
-    //UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-    //                    (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-    //                     UART_CONFIG_PAR_NONE));
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                         UART_CONFIG_PAR_NONE));
 
     //
     // Enable the UART interrupt.
     //
-    //IntEnable(INT_UART0);
-    //UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 
     //
     // Prompt for text to be entered.
     //
-    //UARTSend((unsigned char *)"Enter text: ", 12);
+    UARTSend((unsigned char *)"Enter text: ", 12);
 
     //
     // Loop forever echoing data through the UART.
@@ -199,6 +199,6 @@ main(void)
 		oLED_Message(1, 0, "ADC Ch2", (long)ADC_sample);
 		ADC_sample = ADC_In(3);
 		oLED_Message(1, 2, "ADC Ch3", (long)ADC_sample);
-		SysCtlDelay(SysCtlClockGet() / 1);
+		//SysCtlDelay(SysCtlClockGet() / 1);
     }
 }
