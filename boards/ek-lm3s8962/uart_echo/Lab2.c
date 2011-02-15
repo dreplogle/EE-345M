c473cc6e4e56456fb027dbc3cedbeec72bf23cee
Katy Loeffler <katy.loeffler@gmail.com>
2011-02-15 14:57:27 -0600

    Initial commit.  This is the testing code for lab2.

---
+++ boards/ek-lm3s8962/uart_echo/Lab2.c
@@ -0,0 +1,251 @@
+//*****************************************************************************
+//
+// Lab2 Tester Code.  This file includes four main testing programs, and 
+// associated functions.
+//
+//*****************************************************************************
+
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/adc.h"
+#include "driverlib/fifo.h"
+#include "drivers/rit128x96x4.h"
+#include "string.h"
+#include "drivers/OS.h"
+
+//***********************************************************************
+//
+// Global variables for testing OS
+//
+//***********************************************************************
+unsigned long Count1;   // number of times thread1 loops
+unsigned long Count2;   // number of times thread2 loops
+unsigned long Count3;   // number of times thread3 loops
+unsigned long Count4;   // number of times thread4 loops
+unsigned long Count5;   // number of times thread5 loops
+int NumCreated;
+Sema4Type Free;         // used for mutual exclusion
+Sema4Type Readyc;       // set in background
+Sema4Type Readyd;        // set in background
+int Lost;
+
+//***********************************************************************
+//
+// OS Testing Functions
+//
+//***********************************************************************
+void Thread1(void){
+  Count1 = 0;          
+  for(;;){
+    Count1++;
+    OS_Suspend();      // cooperative multitasking
+  }
+}
+void Thread2(void){
+  Count2 = 0;          
+  for(;;){
+    Count2++;
+    OS_Suspend();      // cooperative multitasking
+  }
+}
+void Thread3(void){
+  Count3 = 0;          
+  for(;;){
+    Count3++;
+    OS_Suspend();      // cooperative multitasking
+  }
+}
+void Thread1b(void){
+  Count1 = 0;          
+  for(;;){
+    Count1++;
+  }
+}
+void Thread2b(void){
+  Count2 = 0;          
+  while(Count2 < 2000){
+    Count2++;
+  }
+  OS_Kill();
+}
+void Thread3b(void){
+  Count3 = 0;          
+  for(;;){
+    Count3++;
+  }
+}
+void BackgroundThread1c(void){   // called at 1000 Hz
+  Count1++;
+  OS_Signal(&Readyc);
+}
+void Thread5c(void){
+  for(;;){
+    OS_Wait(&Readyc);
+    Count5++;   // Count2 + Count5 should equal Count1 
+    Lost = Count1-Count5-Count2;
+  }
+}
+void Thread2c(void){
+  OS_InitSemaphore(&Readyc,0);
+  Count1 = 0;    // number of times signal is called      
+  Count2 = 0;    
+  Count5 = 0;    // Count2 + Count5 should equal Count1  
+  NumCreated += OS_AddThread(&Thread5c,128,3); 
+  OS_AddPeriodicThread(&BackgroundThread1c,1,0); 
+  for(;;){
+    OS_Wait(&Readyc);
+    Count2++;   // Count2 + Count5 should equal Count1
+  }
+}
+
+void Thread3c(void){
+  Count3 = 0;          
+  for(;;){
+    Count3++;
+  }
+}
+void Thread4c(void){ int i;
+  for(i=0;i<64;i++){
+    Count4++;
+    OS_Sleep(10);
+  }
+  OS_Kill();
+  Count4 = 0;
+}
+void BackgroundThread5c(void){   // called when Select button pushed
+  NumCreated += OS_AddThread(&Thread4c,128,3); 
+}
+
+void BackgroundThread1d(void){   // called at 1000 Hz
+static int i=0;
+  i++;
+  if(i==50){
+    i = 0;         //every 50 ms
+    Count1++;
+    OS_bSignal(&Readyd);
+  }
+}
+void Thread2d(void){
+  OS_InitSemaphore(&Readyd,0);
+  Count1 = 0;          
+  Count2 = 0;          
+  for(;;){
+    OS_bWait(&Readyd);
+    Count2++;     
+  }
+}
+void Thread3d(void){
+  Count3 = 0;          
+  for(;;){
+    Count3++;
+  }
+}
+void Thread4d(void){ int i;
+  for(i=0;i<640;i++){
+    Count4++;
+    OS_Sleep(1);
+  }
+  OS_Kill();
+}
+void BackgroundThread5d(void){   // called when Select button pushed
+  NumCreated += OS_AddThread(&Thread4d,128,3); 
+}
+      
+
+//+++++++++++++++++++++++++DEBUGGING CODE++++++++++++++++++++++++
+// ONCE YOUR RTOS WORKS YOU CAN COMMENT OUT THE REMAINING CODE
+// 
+//*******************Initial TEST**********
+// This is the simplest configuration, test this first
+// run this with 
+// no UART interrupts
+// no SYSTICK interrupts
+// no timer interrupts
+// no select interrupts
+// no ADC serial port or oLED output
+// no calls to semaphores
+int testmain1(void){ 
+  OS_Init();           // initialize, disable interrupts
+  NumCreated = 0 ;
+  NumCreated += OS_AddThread(&Thread1,128,1); 
+  NumCreated += OS_AddThread(&Thread2,128,2); 
+  NumCreated += OS_AddThread(&Thread3,128,3); 
+ 
+  OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
+  return 0;             // this never executes
+}
+
+//*******************Second TEST**********
+// Once the initalize test runs, test this 
+// no UART interrupts
+// SYSTICK interrupts, with or without period established by OS_Launch
+// no timer interrupts
+// no select switch interrupts
+// no ADC serial port or oLED output
+// no calls to semaphores
+int testmain2(void){  // testmain2
+  OS_Init();           // initialize, disable interrupts
+  NumCreated = 0 ;
+  NumCreated += OS_AddThread(&Thread1b,128,1); 
+  NumCreated += OS_AddThread(&Thread2b,128,2); 
+  NumCreated += OS_AddThread(&Thread3b,128,3);
+  OS_AddButtonTask(&Thread1,1); 
+ 
+  OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
+  return 0;             // this never executes
+}
+
+//*******************Third TEST**********
+// Once the second test runs, test this 
+// no UART1 interrupts
+// SYSTICK interrupts, with or without period established by OS_Launch
+// Timer2 interrupts, with or without period established by OS_AddPeriodicThread
+// PortF GPIO interrupts, active low
+// no ADC serial port or oLED output
+// tests the spinlock semaphores, tests Sleep and Kill
+int testmain3(void){   // Testmain3
+  Count4 = 0;          
+  OS_Init();           // initialize, disable interrupts
+
+  NumCreated = 0 ;
+  OS_AddButtonTask(&BackgroundThread5c,2);
+  NumCreated += OS_AddThread(&Thread2c,128,2); 
+  NumCreated += OS_AddThread(&Thread3c,128,3); 
+  NumCreated += OS_AddThread(&Thread4c,128,3); 
+  OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
+  return 0;  // this never executes
+}
+
+//*******************Fourth TEST**********
+// Once the third test runs, run this example
+// Count1 should exactly equal Count2
+// Count3 should be very large
+// Count4 increases by 640 every time select is pressed
+// NumCreated increase by 1 every time select is pressed
+
+// no UART interrupts
+// SYSTICK interrupts, with or without period established by OS_Launch
+// Timer interrupts, with or without period established by OS_AddPeriodicThread
+// Select switch interrupts, active low
+// no ADC serial port or oLED output
+// tests the spinlock semaphores, tests Sleep and Kill
+int main(void){   // Testmain4
+  Count4 = 0;          
+  OS_Init();           // initialize, disable interrupts
+  NumCreated = 0 ;
+  OS_AddPeriodicThread(&BackgroundThread1d,100,0); 
+  OS_AddButtonTask(&BackgroundThread5d,2);
+  NumCreated += OS_AddThread(&Thread2d,128,2); 
+  NumCreated += OS_AddThread(&Thread3d,128,3); 
+  NumCreated += OS_AddThread(&Thread4d,128,3); 
+  OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
+  return 0;  // this never executes
+}
+
