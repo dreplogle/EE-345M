35423d4945db8508fe08609ddbbd313c0d80f673
Aravind <akannan@mail.utexas.edu>
2011-04-02 21:41:02 -0500

    Added Fifo functions to ping.h file

--- boards/ek-lm3s8962/drivers/ping.h
+++ boards/ek-lm3s8962/drivers/ping.h
@@ -1,3 +1,6 @@
 void Ping_Init(unsigned long periodicTimer, unsigned long subTimer);
 void pingProducer(void);
 void pingConsumer(void);
+int Ping_Fifo_Put(unsigned long data);
+void Ping_Fifo_Init(void);
+unsigned long Ping_Fifo_Get(void);
