c540c2a53139d873ac6d7f141ecaf8d10d0f9288
Aravind <akannan@mail.utexas.edu>
2011-04-03 19:06:39 -0500

    all registers tested and interrupts work

--- boards/ek-lm3s8962/drivers/ping.c
+++ boards/ek-lm3s8962/drivers/ping.c
@@ -11,10 +11,10 @@
 #include <stdio.h>
 
 #define SYSCTL_RCGC2_R     (*((volatile unsigned long *)0x400FE108)) 
-#define GPIOD_AFSEL_R		(*((volatile unsigned long *)0x40007420)) 
+#define GPIOA_AFSEL_R		(*((volatile unsigned long *)0x40004420)) 
 #define FIVE_USEC 250
 #define PIN_6_WRITE 0x40 
-#define MAX_16_BIT_TCNT 
+#define MAX_16_BIT_TCNT 0xFFFF
 #define NUMBER_OF_CYCLES_IN_USEC 50
 #define SPEED_OF_SOUND 34029 //speed of sound in 10 nm/us units
 #define NUMBER_OF_NM_IN_MM 1000000
@@ -44,7 +44,7 @@ struct buf_st {
   char buf [PING_BUF_SIZE];                           // Buffer
 };
 
-static struct buf_st pingbuf = { 0, 0, };
+struct buf_st pingbuf = { 0, 0, };
 #define PING_BUFLEN ((unsigned short)(pingbuf.in - pingbuf.out))
 
 
@@ -54,7 +54,7 @@ static struct buf_st pingbuf = { 0, 0, };
 // Inputs: size of the FIFO
 // Outputs: none
 void Ping_Fifo_Init(void){
-	OS_InitSemaphore(&Ping_Fifo_Available, 0);
+//	OS_InitSemaphore(&Ping_Fifo_Available, 0);
 	pingbuf.in = 0;
   	pingbuf.out = 0;
 	
@@ -74,7 +74,8 @@ int Ping_Fifo_Put(unsigned long data){
   	p->buf [p->in & (PING_BUF_SIZE - 1)] = data;           // Add data to the transmit buffer.
   	p->in++;
 
-	OS_Signal(&Ping_Fifo_Available);
+
+//	OS_Signal(&Ping_Fifo_Available);
 	return 1;
 
 }
@@ -85,7 +86,11 @@ int Ping_Fifo_Put(unsigned long data){
 // Outputs: buffer entry
 unsigned long Ping_Fifo_Get(void){ 
   	struct buf_st *p = &pingbuf;
-	OS_Wait(&Ping_Fifo_Available);
+//	OS_Wait(&Ping_Fifo_Available);
+	if (PING_BUFLEN == 0)
+	{
+		return 0;
+	}
   	return (p->buf [(p->out++) & (PING_BUF_SIZE - 1)]);
 }
 
@@ -120,8 +125,11 @@ void pingProducer(void)
 	unsigned long startTime = OS_Time();
 	unsigned long endTime = OS_Time();
 
+	TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
 
 	//send a 5 us pulse	on PA6
+	GPIODirModeSet(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_DIR_MODE_OUT);
+
 	IntMasterDisable();
 	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, PIN_6_WRITE);
 	while ( OS_TimeDifference(endTime, startTime) < FIVE_USEC)
@@ -133,13 +141,14 @@ void pingProducer(void)
 
 	
 	//make PA6 an input and set it to input capture
-	GPIODirModeSet(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_DIR_MODE_IN);
-	GPIOD_AFSEL_R |= PIN_6_WRITE;
+//	GPIODirModeSet(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_DIR_MODE_IN);
+	GPIOA_AFSEL_R |= PIN_6_WRITE;
 
 
 	//make Timer 0B an input capture timer and interrupt on edges
-	TimerConfigure(TIMER0_BASE, TIMER_CFG_B_CAP_TIME);
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_16_BIT_PAIR | TIMER_CFG_B_CAP_TIME);
 	TimerControlEvent(TIMER0_BASE, TIMER_B, TIMER_EVENT_BOTH_EDGES);
+	TimerLoadSet(TIMER0_BASE, TIMER_B, 0xFFFF);
 	TimerIntEnable(TIMER0_BASE, TIMER_CAPB_EVENT);
 
 
